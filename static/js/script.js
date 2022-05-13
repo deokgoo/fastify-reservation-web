4a14c178d190f63d93c2778813dd3937f99facaf
deokgoo <kkddgg1001@gmail.com>
2022-05-14 00:25:08 +0900

    feature: landing page API

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,36 @@
 const BTN_INACTIVE = 'btn-light';
 const BTN_ACTIVE = 'btn-success';
 
-const $fitlerAll = document.querySelector('js-filter-all');
-const $fitlerAOS = document.querySelector('js-filter-aos');
-const $fitlerIOS = document.querySelector('js-filter-ios');
+const $fitlerAll = document.querySelector('.js-filter-all');
+const $fitlerAOS = document.querySelector('.js-filter-aos');
+const $fitlerIOS = document.querySelector('.js-filter-ios');
+
+const params = new Proxy(new URLSearchParams(window.location.search), {
+  get: (searchParams, prop) => searchParams.get(prop),
+});
+
+const activeClass = ($el) => {
+  $el.classList.add(BTN_ACTIVE);
+  $el.classList.remove(BTN_INACTIVE);
+  $el.classList.add('active');
+}
+
+const inActiveClass = ($el) => {
+  $el.classList.add(BTN_INACTIVE);
+  $el.classList.remove(BTN_ACTIVE);
+  $el.classList.remove('active');
+}
+
+if(params.os === 'aos') {
+  activeClass($fitlerAOS);
+  inActiveClass($fitlerIOS);
+  inActiveClass($fitlerAll);
+} else if(params.os === 'ios') {
+  activeClass($fitlerIOS);
+  inActiveClass($fitlerAOS);
+  inActiveClass($fitlerAll);
+} else {
+  activeClass($fitlerAll);
+  inActiveClass($fitlerAOS);
+  inActiveClass($fitlerIOS);
+}
